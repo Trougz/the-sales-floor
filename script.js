c04b9400669000883c03bba239d72f9ba3748feb
Trougz <zachtrougakos@gmail.com>
2026-07-24 18:19:37 -0400

    Fix missing quotes around new Apps Script URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SHEETS_URL =https://script.google.com/macros/s/AKfycbx-BfqjKLgveHnFs7cxN5oyoJ7P7xuk0SOaDPNqDhNZ5Klz5kut9voxCPJnDMUgoVv7/exec;
+const SHEETS_URL = 'https://script.google.com/macros/s/AKfycbx-BfqjKLgveHnFs7cxN5oyoJ7P7xuk0SOaDPNqDhNZ5Klz5kut9voxCPJnDMUgoVv7/exec';
 
 const form = document.getElementById('intake-form');
 const success = document.getElementById('success');
